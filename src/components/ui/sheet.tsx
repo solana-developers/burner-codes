202938a3081f5ceb9f1572752f5163adb0b9bd01
nickfrosty <nfrostbutter@gmail.com>
2024-02-19 13:07:32 -0500

    refactor: sheet style

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -63,7 +63,7 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      <SheetPrimitive.Close className="absolute right-2 top-0 p-2 hover:bg-gray-200 rounded-full ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="w-4 h-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
@@ -106,7 +106,7 @@ const SheetTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SheetPrimitive.Title
     ref={ref}
-    className={cn("text-xl font-semibold text-foreground", className)}
+    className={cn("text-2xl font-semibold text-foreground", className)}
     {...props}
   />
 ));
