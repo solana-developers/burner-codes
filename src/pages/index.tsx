e1bdb36165952b8240bf1674ccf8b3ee708612d6
nickfrosty <nfrostbutter@gmail.com>
2024-02-19 11:18:05 -0500

    feat: loading home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ const seo: NextSeoProps = {
 };
 
 export default function Page() {
-  const { burner, balance, setBalance, cluster, connection } =
+  const { burner, balance, setBalance, cluster, connection, loading } =
     useGlobalContext();
 
   // track the needed state for the new various dialogs
@@ -76,24 +76,23 @@ export default function Page() {
     toast.success(message);
   }
 
-  // if (loading) {
-  //   return (
-  //     <DefaultLayout seo={seo}>
-  //       <main className="container max-w-lg py-10 space-y-8 md:py-20">
-  //         loading burner wallet
-  //       </main>
-  //     </DefaultLayout>
-  //   );
-  // }
+  if (loading) {
+    return (
+      <DefaultLayout seo={seo}>
+        <main className="container max-w-lg py-10 space-y-8 text-center md:py-20">
+          <h1 className="text-4xl">Loading burner wallet</h1>
+          <LoadingSpinner visible={true} width={70} className="mx-auto" />
+        </main>
+      </DefaultLayout>
+    );
+  }
 
   return (
     <DefaultLayout seo={seo}>
       <main className="container max-w-lg py-10 space-y-8 md:py-20">
         <section className="space-y-6">
-          <section className="space-y-2">
-            <h1 className="text-4xl text-center">
-              {formatLamportsToSol(balance)}
-            </h1>
+          <section className="space-y-2 text-center">
+            <h1 className="text-4xl">{formatLamportsToSol(balance)}</h1>
 
             <p className="text-center text-gray-500">on Solana {cluster}</p>
           </section>
