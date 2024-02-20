b2f3bc32c17053792375d36e4e781b0ac9d564e1
nickfrosty <nfrostbutter@gmail.com>
2024-02-20 12:43:31 -0500

    feat: allow app dir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
