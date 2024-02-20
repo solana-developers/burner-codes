c7cbd7852d4d9e222980753f6e395d521eca5646
nickfrosty <nfrostbutter@gmail.com>
2024-02-20 15:19:05 -0500

    refactor: static scan url

--- src/pages/scan.tsx
+++ src/pages/scan.tsx
@@ -1,13 +1,19 @@
-"use client";
-
 import type { NextSeoProps } from "next-seo";
 import DefaultLayout from "@/layouts/default";
 import { useCallback, useEffect, useState } from "react";
 import { useGlobalContext } from "@/contexts/GlobalContext";
 import { onQRScanError, qrScannerConfig } from "@/lib/scanner";
 import { Html5QrcodeScanner, QrcodeSuccessCallback } from "html5-qrcode";
-import { getSolanaAddress } from "@/lib/solanaPay";
+import { getSolanaAddress } from "@/lib/solana/helpers";
 import { TransactionRequestURL, parseURL } from "@solana/pay";
+import { proxySolanaPayRequest } from "@/lib/solana/transactionRequest";
+import toast from "react-hot-toast";
+
+const SOLANA_PAY_URL = {
+  left: "https://tug-of-war-solana-pay.vercel.app/api/transaction?amount=0.001&instruction=pull_right&network=devnet",
+  right:
+    "https://tug-of-war-solana-pay.vercel.app/api/transaction?amount=0.001&instruction=pull_left&network=devnet",
+};
 
 // define page specific seo settings
 const seo: NextSeoProps = {
@@ -20,9 +26,9 @@ const seo: NextSeoProps = {
 export default function Page() {
   const {
     // comment for better diffs
-    cluster,
     connection,
-    sendTransaction,
+    prepareTransaction,
+    burner,
   } = useGlobalContext();
 
   const [isOpen, setIsOpen] = useState<boolean>(true);
@@ -105,8 +111,50 @@ export default function Page() {
           </section>
 
           <div
-            onClick={() => sendTransaction("temp")}
             className="p-3 overflow-hidden card min-w-[400px] min-h-[475px]"
+            onClick={() =>
+              prepareTransaction(async () => {
+                const solanaPayData = await proxySolanaPayRequest(
+                  SOLANA_PAY_URL["right"],
+                  burner!.publicKey.toBase58(),
+                );
+
+                console.log("\nfinal data:");
+                console.log(solanaPayData);
+
+                // toast(`final:` + JSON.stringify(solanaPayData));
+
+                return {
+                  transaction: solanaPayData.post.transaction,
+                  error: solanaPayData.post.error,
+                  // todo: add the SolanaPay payload info in here, and typed with a discriminator
+                  // payload: {}
+                };
+              })
+            }
+            // onClick={() =>
+            //   prepareTransaction(async () => {
+            //     const transaction = await createTransfer(
+            //       connection,
+            //       burner!.publicKey,
+            //       {
+            //         recipient: new PublicKey(
+            //           "GQuioVe2yA6KZfstgmirAvugfUZBcdxSi7sHK7JGk3gk",
+            //         ),
+            //         amount: BigNumber(12.0) as any,
+            //         splToken: undefined, // or PublicKey of a token's mint
+            //         reference: undefined, // or PublicKey[]
+            //         // label: undefined,
+            //         // message: undefined,
+            //         memo: "this is the memo text for this solana pay transaction",
+            //       },
+            //     );
+
+            //     return {
+            //       transaction,
+            //     };
+            //   })
+            // }
           >
             <div id="scanner">Waiting for camera...</div>
           </div>
